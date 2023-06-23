a74273b996f3d106076c76fb63707319de31ede6
Marta Stefaniak <vocado7@gmail.com>
2023-06-23 19:43:03 +0200

    add content from json server to about us page

--- src/app/(custom_layout)/about/page.tsx
+++ src/app/(custom_layout)/about/page.tsx
@@ -1,7 +1,56 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {apiCall} from "@/api/api";
+
+interface Paragraph {
+    type: "paragraph",
+    content: string
+}
+
+interface List {
+    type: "list",
+    content: string[]
+}
+
+interface Content {
+    id: number,
+    mainTitle: string,
+    subtitle: string,
+    image: string,
+    texts: Array<Paragraph | List>
+}
+
 function Page() {
+    const [content, setContent] = useState<Content | undefined>(undefined)
+
+    useEffect(() => {
+        apiCall<Content>({endpoint: 'contents/1'})
+            .then(setContent)
+    }, [])
+
+    if (content === undefined) {
+        return <h2>Loading...</h2>
+    }
+
     return (
         <>
-            <h1 className="text-center text-5xl mb-5">About us</h1>
+            <h1 className="text-center text-5xl mb-5">{content.mainTitle}</h1>
+            <div className="m-5">
+                <h3>{content.subtitle}</h3>
+                {content.texts.map((text) => (
+                    <>{text.type === "paragraph" ? (
+                        <p>{text.content}</p>
+                    ) : (
+                        <ul>
+                            {text.content.map((item) => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
+                    )}</>
+                ))}
+            </div>
+
         </>
     );
 }
