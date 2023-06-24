84982e1e40a9ba63432798ae7f0276e483598ed6
Marta Stefaniak <vocado7@gmail.com>
2023-06-24 19:28:06 +0200

    add sticky navbar

--- src/app/(custom_layout)/page.tsx
+++ src/app/(custom_layout)/page.tsx
@@ -1,8 +1,22 @@
+import About from "@/components/About";
+import Employees from "@/components/Employees";
+import FormContact from "@/components/FormContact";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Home
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <section id="home" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1>Home</h1>
+            </section>
+            <section id="about" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <About/>
+            </section>
+            <section id="employees" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <Employees/>
+            </section>
+            <section id="form" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <FormContact/>
+            </section>
+        </main>
+    )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,14 @@
-import Link from "next/link";
 import Image from "next/image";
 
 function Navbar() {
     return (
-        <nav className="group flex justify-items-center gap-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <Link
-                className="pointer-events-none lg:pointer-events-auto lg:p-0"
-                href="/">
-                <Image src="/logo.png" alt="Azeno Company logo" width="50" height="63" />
-            </Link>
-            <Link
-                className="pointer-events-none lg:pointer-events-auto lg:p-0"
-                href="/">Home</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/about">About us</Link>
-            <Link href="/employees">Employees</Link>
+        <nav
+            className="sticky top-0 z-50 flex justify-center-items gap-10 group bg-gray-800 bg-opacity-70 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <a href="#home"><Image alt="Azeno Company logo" src="/logo.png" width="50" height="63"/></a>
+            <a href="#home">Home</a>
+            <a href="#about">About us</a>
+            <a href="#employees">Employees</a>
+            <a href="#form">Contact</a>
         </nav>
     );
 }
